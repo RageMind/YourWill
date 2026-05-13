c6e7453c9f3bd6c92562e3c675ebff3d1b3856a8
Мир Рейтер <69878731+RageMind@users.noreply.github.com>
2026-05-13 05:24:45 +0300

    Add world config

---
+++ src/config.js
@@ -0,0 +1,37 @@
+window.YW = window.YW || {};
+
+window.YW.CONFIG = {
+  width: 180,
+  height: 128,
+  tile: 24,
+  colors: {
+    background: '#07110d',
+    deepWater: '#12394a',
+    water: '#1e6271',
+    shallowWater: '#4aa0a0',
+    foam: '#9dd6c8',
+    sandDark: '#b98e4f',
+    sand: '#d2aa62',
+    sandLight: '#e4c37a',
+    grassDark: '#416f35',
+    grass: '#5b8a3f',
+    grassLight: '#779e49',
+    meadow: '#8fb555',
+    meadowLight: '#acd16a',
+    forestDark: '#1c3c29',
+    forest: '#274d2d',
+    forestLight: '#326633',
+    rockDark: '#4c514a',
+    rock: '#72786e',
+    rockLight: '#a7aa98',
+    trunkDark: '#3b2416',
+    trunk: '#744526',
+    leafDeep: '#183322',
+    leafDark: '#2f6134',
+    leaf: '#558b3f',
+    leafLight: '#9bc45c',
+    berry: '#c24746',
+    flowerYellow: '#f0c763',
+    flowerWhite: '#f6ead0'
+  }
+};
